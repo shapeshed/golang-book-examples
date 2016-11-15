56e2f6c321c7e07e1d8d1cea3fff9f06d36183e1
George Ornbo <george@shapeshed.com>
2016-11-15 21:48:10 +0000

    Update JSON string to hello world

--- hour20/example03.go
+++ hour20/example03.go
@@ -10,7 +10,7 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Write([]byte("{\"foo\": \"bar\"}"))
+	w.Write([]byte("{\"hello\": \"world\"}"))
 }
 
 func main() {
