282652b028b67fbb6091c4b7e2d46a385f1963a1
George Ornbo <george@shapeshed.com>
2016-11-15 17:02:55 +0000

    Simplify example06

--- hour19/example06.go
+++ hour19/example06.go
@@ -5,29 +5,17 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"os"
 	"time"
 )
 
 func main() {
-	debug := os.Getenv("DEBUG")
 
 	client := &http.Client{
 		Timeout: 50 * time.Millisecond,
 	}
 	request, err := http.NewRequest("GET", "https://ifconfig.co", nil)
-	request.Header.Add("Accept", "application/json")
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	if debug == "1" {
-		debugRequest, err := httputil.DumpRequestOut(request, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s", debugRequest)
 	}
 
 	response, err := client.Do(request)
@@ -35,14 +23,6 @@ func main() {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
-
-	if debug == "1" {
-		debugResponse, err := httputil.DumpResponse(response, true)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s", debugResponse)
-	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
