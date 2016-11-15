bbb7541347dfdfb51252636ce0f88809b7543e8d
George Ornbo <george@shapeshed.com>
2016-11-15 10:21:48 +0000

    Add hour 19 examples

---
+++ hour19/example01.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	response, err := http.Get("https://ifconfig.co/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+
+}
---
+++ hour19/example02.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", "https://ifconfig.co", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+
+}
