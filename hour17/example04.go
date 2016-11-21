69276397003fbebf1b3c7a3a166c869b2f2fc834
George Ornbo <george@shapeshed.com>
2016-11-21 12:07:16 +0000

    Remove duplicate example and rename

--- hour17/example04.go
+++ hour17/example04.go
@@ -3,9 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	flag.Usage = func() {
+		usageText := `Usage example05 [OPTION]
+An example of customising usage output
+
+  -s, --s         example string argument, default: String help text
+  -i, --i         example integer argument, default: Int help text
+  -b, --b         example boolean argument, default: Bool help text`
+		fmt.Fprintf(os.Stderr, "%s\n", usageText)
+
+	}
 	s := flag.String("s", "Hello world", "String help text")
 	i := flag.Int("i", 1, "Int help text")
 	b := flag.Bool("b", true, "Bool help text")
--- hour17/example05.go
+++ hour17/example05.go
@@ -4,24 +4,74 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
+func flagUsage() {
+	usageText := `example06 is an example cli tool.
+        
+Usage:
+
+example06 command [arguments]
+
+The commands are:
+
+uppercase  uppercase a string 
+lowercase  lowercase a string
+
+Use "example06 [command] --help" for more information about a command.`
+	fmt.Fprintf(os.Stderr, "%s\n\n", usageText)
+}
+
+func uppercaseCmdUsage() {
+	usageText := `usage: uppercase [-s]
+        
+    -s
+        A string to be uppercased`
+	fmt.Fprintf(os.Stderr, "%s\n\n", usageText)
+}
+
+func lowercaseCmdUsage() {
+	usageText := `usage: lowercase [-s]
+        
+    -s
+        A string to be lowercased`
+	fmt.Fprintf(os.Stderr, "%s\n\n", usageText)
+}
+
 func main() {
-	flag.Usage = func() {
-		usageText := `Usage example05 [OPTION]
-An example of customising usage output
 
-  -s, --s         example string argument, default: String help text
-  -i, --i         example integer argument, default: Int help text
-  -b, --b         example boolean argument, default: Bool help text`
-		fmt.Fprintf(os.Stderr, "%s\n", usageText)
+	flag.Usage = flagUsage
 
+	uppercaseCmd := flag.NewFlagSet("uppercase", flag.ExitOnError)
+	uppercaseCmd.Usage = uppercaseCmdUsage
+	lowercaseCmd := flag.NewFlagSet("lowercase", flag.ExitOnError)
+	lowercaseCmd.Usage = lowercaseCmdUsage
+
+	if len(os.Args) == 1 {
+		flag.Usage()
+		return
 	}
-	s := flag.String("s", "Hello world", "String help text")
-	i := flag.Int("i", 1, "Int help text")
-	b := flag.Bool("b", true, "Bool help text")
-	flag.Parse()
-	fmt.Println("value of s:", *s)
-	fmt.Println("value of i:", *i)
-	fmt.Println("value of b:", *b)
+
+	switch os.Args[1] {
+	case "uppercase":
+		s := uppercaseCmd.String("s", "", "A string of text to be uppercased")
+		uppercaseCmd.Parse(os.Args[2:])
+		if *s == "" {
+			uppercaseCmd.Usage()
+			return
+		}
+		fmt.Println(strings.ToUpper(*s))
+	case "lowercase":
+		s := lowercaseCmd.String("s", "", "A string of text to be lowercased")
+		lowercaseCmd.Parse(os.Args[2:])
+		if *s == "" {
+			lowercaseCmd.Usage()
+			return
+		}
+		fmt.Println(strings.ToLower(*s))
+	default:
+		flag.Usage()
+	}
+
 }
