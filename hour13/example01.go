415396ab1d66dbdb1605f561526ca2abf1e1d3ba
George Ornbo <george@shapeshed.com>
2016-11-24 14:08:22 +0000

    Hour 13 examples

---
+++ hour13/example01.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+)
+
+func main() {
+	s := "Ti esrever dna ti pilf nwod gniht ym tup I"
+	fmt.Println(stringutil.Reverse(s))
+}
