19aa0e4519f149a78a51a5ad9012749dd65222e3
George Ornbo <george@shapeshed.com>
2017-05-21 22:46:14 +0100

    Create folders for examples

--- hour13/example02/example02.go
+++ hour13/example02/example02.go
@@ -3,24 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/golang/example/stringutil"
-	"github.com/shapeshed/darksky"
-	"log"
 )
 
 func main() {
 	s := "Ti esrever dna ti pilf nwod gniht ym tup I"
 	fmt.Println(stringutil.Reverse(s))
-
-	params := darksky.RequestParams{
-		Key:       "17b1e8cae7b654290659b438557def7e",
-		Latitude:  52.847875,
-		Longitude: -0.664397,
-		Units:     "si",
-	}
-
-	forecast, err := darksky.Get(&params)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(forecast.Currently.Temperature)
 }
